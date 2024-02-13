fe174761b4983e6ed2ae0673ed13f544b967fb9c
krzers <tyhffggfg@gmail.com>
2024-02-13 14:28:24 +0700

    1234

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect a,Rect b){
+    double width = max(0.0, min(a.x + a.w, b.x + b.w) - max(a.x, b.x));
+    double height = max(0.0, min(a.y, b.y) - max(a.y - a.h, b.y - b.h));
+    return width * height;
 
 }
